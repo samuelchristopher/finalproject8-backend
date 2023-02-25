66748996156b279849062399d51dc4625bc4cba0
samuelchristopher <samuel.christopher.santo@gmail.com>
2023-02-25 22:22:05 +0700

    feat: add commented routers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const { sequelize } = require('./models');
 
 const router = require('./routes/router');
+// const categoriesRouter = require('./routes/categoriesRouter');
+// const productsRouter = require('./routes/productsRouter');
+// const productImgRouter = require('./routes/productImgRouter');
+// const reviewRouter = require('./routes/reviewRouter');
+// const ordersRouter = require('./routes/ordersRouter');
 
 const app = express();
 
@@ -22,5 +27,12 @@ sequelize
   });
 
 app.use('/', router);
+// app.use('/categories', categoriesRouter);
+// app.use('/products', productsRouter);
+// app.use('/product-images', productImgRouter);
+// app.use('/reviews', reviewRouter);
+// app.use('/orders', ordersRouter);
 
-app.listen(process.env.SERVER_PORT, () => {console.log(`Server Running on port: ${process.env.SERVER_PORT}`)});
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server Running on port: ${process.env.SERVER_PORT}`);
+});
