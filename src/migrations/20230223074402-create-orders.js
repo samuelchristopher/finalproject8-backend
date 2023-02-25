e43cc033b24dc06556ac1f5b14f322678d8f2cd7
samuelchristopher <samuel.christopher.santo@gmail.com>
2023-02-25 22:20:39 +0700

    fix: restructuring orders and order_informations

--- src/migrations/20230223074402-create-orders.js
+++ src/migrations/20230223074402-create-orders.js
@@ -20,6 +20,58 @@ module.exports = {
       status: {
         type: Sequelize.STRING,
       },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      first_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      last_name: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      provinsi: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      kabkota: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      kecamatan: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      kelurahan: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      postal_code: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      phone_number: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      other_desc: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      shipping_method: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      payment_method: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
