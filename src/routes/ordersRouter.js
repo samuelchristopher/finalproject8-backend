d3e03c7c627e7a91fa058b910d13a56dd519580c
samuelchristopher <samuel.christopher.santo@gmail.com>
2023-03-25 10:59:58 +0700

    fix: set GET no auth

--- src/routes/ordersRouter.js
+++ src/routes/ordersRouter.js
@@ -5,12 +5,8 @@ const authHelper = require('../helpers/auth');
 const router = express.Router();
 
 router.post('/', ordersController.addOrder);
-router.get('/', authHelper.verifyToken, ordersController.getOrders);
-router.get(
-  '/:idOrCode',
-  authHelper.verifyToken,
-  ordersController.getOrderByIdOrCode
-);
+router.get('/', ordersController.getOrders);
+router.get('/:idOrCode', ordersController.getOrderByIdOrCode);
 router.put(
   '/:idOrCode',
   authHelper.verifyToken,
